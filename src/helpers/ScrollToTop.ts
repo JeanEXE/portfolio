b67e0f0f6db85630866227319b45f76890892130
Jean Reis <jeansreiss@gmail.com>
2022-05-29 15:55:14 -0300

    resetando scroll no reaload da pagina // .env gitIgnore // useDimensions.tsx > useDimensions.ts

--- src/helpers/ScrollToTop.ts
+++ src/helpers/ScrollToTop.ts
@@ -4,6 +4,10 @@ import { useLocation } from "react-router-dom";
 export default function ScrollToTop(): null {
     const { pathname } = useLocation();
 
+    window.onunload = function () {
+        window.scrollTo(0, 0);
+    }
+
     useEffect(() => {
         window.scrollTo(0, 0);
     }, [pathname]);

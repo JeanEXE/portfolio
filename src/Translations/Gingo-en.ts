234aff5313374b29768adfd0c607286089b1f0e3
unknown <jeansreiss@gmail.com>
2022-08-18 23:21:30 -0300

    tradutor :: Gingo em ingles // Gingo Business em ingles

--- src/Translations/Gingo-en.ts
+++ src/Translations/Gingo-en.ts
@@ -1,34 +1,34 @@
-const DESC_PROJETO = `Gingo é um aplicativo de delivery e chat em tempo real, onde os usuários podem conversar a qualquer momento com estabelecimentos ou entre sí.
-     O app foi desenvolvido em React Native, o backend do marketplace em Magento(PHP) e do chat em Node.js. Trabalhei neste projeto desde seu início até a publicação nas stores (Android/iOS).
-     Atuei no desenvolvimento das telas e funcionalidades do marketplace no frontend, desde o cadastro/login até a finalização de um pedido, mas meu maior foco foi no desenvolvimento do chat, onde atuei no backend (Node.js) utilizando express e Socket.io e em todo o frontend (React Native).
+const DESC_PROJETO = `Gingo is a real-time delivery and chat application, where users can chat at any time with establishments or with each other.
+    The app was developed in React Native, the backend of the marketplace in Magento(PHP) and the chat in Node.js. I worked on this project from its beginning until its publication in stores (Android/iOS).
+    I worked on the development of the marketplace's screens and functionalities on the frontend, from the registration/login to the checkout, but my main focus was on the development of the chat, where I worked on the backend (Node.js) using express and Socket.io and across the frontend (React Native).
 `
 
-const DESAFIO = `Meu maior desafio foi com a construção do chat, tanto no back quanto no frontend, devido a sua construção offline first no frontend e seu sistema de garantir que nenhuma mensagem seja perdida devido a oscilação de internet.
-     No backend utilizamos Socket.io pra executar envio e recebimento em tempo real, com sistema de pendencias salvos no MongoDB. 
-     No app utilizamos o banco de dados SQLite pra salvar as mensagens localmente e um sistema de criptografia p2p utilizando RSA e AES.
+const DESAFIO = `My biggest challenge was with the construction of the chat, both in the back and in the frontend, due to its offline first construction in the frontend and its system of ensuring that no messages are lost due to internet oscillation.
+    On the backend, we use Socket.io to perform sending and receiving in real time, with a pending system saved in MongoDB.
+    In the app we use the SQLite database to save messages locally and a p2p encryption system using RSA and AES.
 `
 
-const APRENDIZADO = `Com esse projeto aprendi a usar express e socket no backend com Node. 
-    Aprendi como funciona todo o ciclo de vida do React Native, sua componentização tanto em componente de classe quanto em componente de função.
-    Após ter problemas com performance, procurei entender melhor como a arquitetura e gerenciamento de estados do react funciona. Com isso aprendi a gerenciar estados de forma mais controlada pra evitar vazamentos de memória, e a utilizar o React MEMO para evitar renderizações desnecessárias.
+const APRENDIZADO = `With this project I learned to use express and socket on the backend with Node.
+    I learned how the entire lifecycle of React Native works, its componentization in both a class component and a function component.
+    After having problems with performance, I tried to better understand how react's architecture and state management works. With that I learned to manage states in a more controlled way to avoid memory leaks, and to use React MEMO to avoid unnecessary rendering.
 -Redux
 -Hooks
--CI/CD utilizando codepush    
+-CI/CD using codepush    
 `
 
 const TRANSLATION = {
     'prj1.description': DESC_PROJETO,
     'prj1.challenge': DESAFIO,
     'prj1.learned': APRENDIZADO,
-    'prj1.feature1.description': `Participei do desenvolvimento de todo o mecanismo de push, desde o desenvolvimendo no servidor node onde é efetuado o envio até a parte do recebimento no aplicativo, no Android foi utilizado o Firebase e no iOS utilizado o APNS.
-    O Sistema de recebimento no aplicativo iOS foi uma parte bem desafiadora de ser feita, pois havia a necessidade de executar funções do aplicativo enquanto ele está fechado, como por exemplo, descriptografar a mensagem que chegou via notificação. 
-    Foi nesse momento que precisei aprender a desenvolver em Swift, criando funções de inserir/deletar dados no SQLite, requisições REST, criptografia/descriptografia, etc...
-No android essas funções foram desenvolvidas em javascript.
+    'prj1.feature1.description': `I participated in the development of the entire push mechanism, from the development on the node server where the sending is made to the receiving part of the application, on Android Firebase was used and on iOS, APNS was used.
+    The Receiving System in the iOS application was a very challenging part to do, as there was a need to perform functions of the application while it is closed, such as decrypting the message that arrived via notification.
+    It was at that moment that I needed to learn to develop in Swift, creating functions to insert/delete data in SQLite, REST requests, encryption/decryption, etc...
+On android these functions were developed in javascript.
 `,
-    'prj1.feature2.description': `Desenvolvi o componente de visualização de imagem em React Native, e todo o processo de envio/download utilizando Socket.io, Amazon s3 e SQLite.`,
-    'prj1.feature3.description': `Desenvolvi o componente de gravação e reprodução de áudio em React Native utilizando "react-native-audio-recorder-player", e todo o processo de envio/download utilizando Socket.io, Amazon s3 e SQLite.`,
-    'prj1.feature4.description': `Desenvolvi o componente de visualização de vídeo em React Native utilizando "react-native-create-thumbnail" e "react-native-file-viewer", e todo o processo de envio/download utilizando Socket.io, Amazon s3 e SQLite.`,
-    'prj1.feature5.description': `Desenvolvi o componente de envio de arquivos em React Native utilizando "react-native-file-viewer", e todo o processo de envio/download utilizando Socket.io, Amazon s3 e SQLite.`
+    'prj1.feature2.description': `I developed the image preview component in React Native, and the entire upload/download process using Socket.io, Amazon s3 and SQLite.`,
+    'prj1.feature3.description': `I developed the audio recording and playback component in React Native using "react-native-audio-recorder-player", and the entire upload/download process using Socket.io, Amazon s3 and SQLite.`,
+    'prj1.feature4.description': `I developed the video viewing component in React Native using "react-native-create-thumbnail" and "react-native-file-viewer", and the entire upload/download process using Socket.io, Amazon s3 and SQLite.`,
+    'prj1.feature5.description': `I developed the file upload component in React Native using "react-native-file-viewer", and the entire upload/download process using Socket.io, Amazon s3 and SQLite.`
 }
 
 export default TRANSLATION
--- src/Translations/GingoBusiness-en.ts
+++ src/Translations/GingoBusiness-en.ts
@@ -1,7 +1,7 @@
 const TRANSLATION = {
-    'prj2.description': `Gingo Business é um aplicativo de gestão de loja/pedidos e chat em tempo real, onde os estabelecimentos conseguem ter controle dos pedidos, editar ou incluir produtos no catálogo e entrar em contato com usuários a qualquer momento.
-    O app foi desenvolvido em React Native, o backend do marketplace em Magento(PHP) e do chat em Node.js. Trabalhei neste projeto desde seu início até a publicação nas stores (Android/iOS).
-    Atuei no desenvolvimento das telas e funcionalidades do marketplace no frontend, como por exemplo o cadastro/login, tela de incluir produtos no catálogo do estabelecimento, funcionalidade de frete por distância, definição de horários de abertura/fechamento da loja, etc...
+    'prj2.description': `Gingo Business is a store/order management and real-time chat application, where establishments can control orders, edit or include products in the catalog and contact users at any time.
+    The app was developed in React Native, the backend of the marketplace in Magento(PHP) and the chat in Node.js. I worked on this project from its beginning until its publication in stores (Android/iOS).
+    I worked on the development of frontend marketplace screens and functionalities, such as registration/login, screen to include products in the establishment's catalog, distance shipping functionality, definition of store opening/closing times, etc...
 `
 }
 
